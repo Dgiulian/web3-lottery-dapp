68a26461f18023363fb68d850967b5d6fe14049f
Diego Giuliani <giuliani.diego@gmail.com>
2022-09-17 13:12:19 -0300

    Update Navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -8,11 +8,14 @@ const Navbar = (props: Props) => {
   const address = useAddress();
   return (
     <div className="p-4 flex items-center">
-      <div className="w-16 h-16 rounded-full bg-amber-500"></div>
+      <div className="w-16 h-16 rounded-full bg-cyan-800"></div>
       <div className="flex align-center justify-between w-full">
-        <div className="flex flex-col ml-4">
+        <div className="flex flex-col justify-center ml-4">
           <span className="text-xl capitalize">Next Generation Lottery </span>
-          {address && <span className="text-xs">{address}</span>}
+
+          <p className="text-xs">
+            {address ? address : "Login with metamask to start winning"}
+          </p>
         </div>
         <ConnectMetamaskButton />
       </div>
