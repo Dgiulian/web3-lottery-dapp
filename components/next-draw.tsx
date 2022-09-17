ac3695cbaa503f0273acd9804a13eee0bb22ee9e
Diego Giuliani <giuliani.diego@gmail.com>
2022-09-17 13:18:55 -0300

    Update responsive styles

--- components/next-draw.tsx
+++ components/next-draw.tsx
@@ -21,8 +21,8 @@ const NextDraw = ({ contract }: Props) => {
     <div className="border-gray-500 w-full bg-slate-700 p-6 rounded-md ">
       <h1 className="text-5xl text-center">The Next Draw</h1>
 
-      <div className="flex justify-center space-x-4 mt-4">
-        <div className="bg-slate-900 w-1/2 p-4 border border-slate-500 flex flex-col rounded-md">
+      <div className="flex justify-center space-y-2 md:space-y-0 md:space-x-4 mt-4 flex-col md:flex-row">
+        <div className="bg-slate-900 w-full md:w-1/2 p-4 border border-slate-500 flex flex-col rounded-md">
           <div className="">Total Pool </div>
           {currentWinningReward && (
             <div className="font-bold text-xl">
@@ -31,7 +31,7 @@ const NextDraw = ({ contract }: Props) => {
             </div>
           )}
         </div>
-        <div className="bg-slate-900 w-1/2 p-4 border border-slate-500 flex flex-col rounded-md">
+        <div className="bg-slate-900  w-full md:w-1/2 p-4 border border-slate-500 flex flex-col rounded-md">
           <div className="">Tickets Remaining</div>
           {remainingTickets && (
             <div className="font-bold text-xl">
